30b1b0edd23da2acf23df7bf4d66fdc83bd537ef
lvanness7690 <121961344+lvanness7690@users.noreply.github.com>
2024-01-29 18:57:15 -0500

    fixing

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -59,7 +59,7 @@ router.post('/login', async (req, res) => {
     try {
         // Find user by username or email
         const user = await User.findOne({
-            $or: [{ username }, { email: username }],
+            $or: [{ username:username }, { email: username }],
         });
 
         if (!user) {
